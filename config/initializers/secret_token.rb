423fce842de1836c1906a6d18d97538aa6d037c8
Eric K Wu <ekuan.wu@gmail.com>
2012-09-06 13:58:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wespot::Application.config.secret_token = '602c8a128ab3f4edec8e712ed4efd141a4e0d2277bc1782e830cc3bc6c78240a0fda8571b410d9a7a9e6d5c6c63ed95070d6ee9240cf14aa14ec624a30dd26fe'
